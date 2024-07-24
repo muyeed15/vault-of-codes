9fccfba6f09e3e9d336175581558719f53ee7f58
Syed Abdullah Al Muyeed <2230324@iub.edu.bd>
2024-07-24 20:16:22 +0600

    Remove unused code

--- CSE201/lcm_extractor.py
+++
@@ -1,7 +0,0 @@
-def lcm(number1, number2):
-    maximum = max(number1, number2)
-
-    while True:
-        if (maximum %  number1 == 0) and (maximum % number2 == 0):
-            return maximum
-        maximum += 1
