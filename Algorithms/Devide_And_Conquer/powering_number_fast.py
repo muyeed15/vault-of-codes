44e910218e334e2f0f6250e79d9263f97cd60cbb
Syed Abdullah Al Muyeed <muyeed.al.abdullah@gmail.com>
2024-08-14 20:02:40 +0600

    Update powering_number_fast.py

--- Algorithms/Devide_And_Conquer/powering_number_fast.py
+++ Algorithms/Devide_And_Conquer/powering_number_fast.py
@@ -1,13 +1,17 @@
 def power(a, b):
+    if b < 0:
+        a = 1/a
+        b = -b
+    
     if b == 0:
         return 1
     
     elif b % 2 == 0:
-        x = power(a, int(b/2))
+        x = power(a, float(int(b/2)))
         return x*x
     
     else:
-        x = power(a, int(b/2))
+        x = power(a, float(int(b/2)))
         return x*x*a
     
 
